d672ddc30cae82c1d5577538e6837cf62cae7040
Nitish Nagpal <nitishnagpal@Nitishs-MacBook-Air.local>
2018-08-01 22:30:20 +0530

    Set up Start script and Heorku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+const port = process.env.port || 3000;
 
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -22,4 +23,6 @@ app.get('/about',(req,res)=>{
   res.render(`about.hbs`,{ pageTitle : 'About Page'});
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);;
+});
